47265d13a1a3ef631f8c2449d4d2b8632fd77b81
Adrian Vide <adrian.vide.paris@gmail.com>
2020-05-08 13:27:32 +0200

    CBox test

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CommentBox from '../CommentBox';
+import { mount } from 'enzyme';
+
+let wrapped
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+})
+
+afterEach(() => {
+    wrapped.unmount();
+})
+
+it('has a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+})
+
+it('has a text area where useres can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+}) 

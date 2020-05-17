002ce44d32889430f63abcfdfbd6f75ea6d12bd0
Adrian Vide <adrian.vide.paris@gmail.com>
2020-05-17 13:01:36 +0200

    integration test

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Root from './Root';
+import App from './App';
+
+it('can fetch a list of comments and display them', () => {
+    //Attempt to render the entire APP
+
+    //Find the 'fecthComments' button and click it
+
+    //Expect to find the 500 comments
+    
+    
+    expect
+})
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import * as actions from '../actions'
+import * as actions from "../actions";
 
 class CommentBox extends Component {
   state = {
@@ -13,21 +13,23 @@ class CommentBox extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-   
-    
-    this.props.saveComment(this.state.comment)
+
+    this.props.saveComment(this.state.comment);
 
     this.setState({ comment: "" });
   };
   render() {
     return (
-      <form onSubmit={this.handleSubmit} action="">
-        <h4>Add a Comment</h4>
-        <textarea onChange={this.handleChange} value={this.state.comment} />
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit} action="">
+          <h4>Add a Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
     );
   }
 }

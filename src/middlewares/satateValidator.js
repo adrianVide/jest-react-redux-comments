2a40dc1beea5e00773a3a56b1e8bd93e9402f976
Adrian Vide <adrian.vide.paris@gmail.com>
2020-06-02 18:31:08 +0200

    92

--- src/middlewares/satateValidator.js
+++ src/middlewares/satateValidator.js
@@ -3,4 +3,6 @@ import stateSchema from './stateSchema';
 
 export default ({ dispatch, getState }) => (next) => (action) => {
     next(action);
+
+    console.log(tv4.validate(getState(), stateSchema));
 }
